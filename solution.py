78ac41586b619ff8cdcc6b4f3948c2165dd4ab4a
Daniel Andres Pinto <Dapinto7@misena.edu.co>
2018-07-05 13:30:59 -0500

    commit inicial

---
+++ solution.py
@@ -0,0 +1,37 @@
+print('Ingresar un numero')
+n = int(input())
+
+def row(n, min):
+  v = []
+  count = n
+  while count > 1:
+    if min > count:
+      v.append(min)
+    else:
+      v.append(count)
+    count -= 1
+
+  while count <= n:
+    if min > count:
+      v.append(min)
+    else:
+      v.append(count)
+    count += 1
+
+  return v
+
+def colum(n):
+
+  m = []
+  count = n
+
+  while count > 1:
+    m.append(row(n, count))
+    count -= 1
+
+  while count <= n:
+    m.append(row(n, count))
+    count += 1
+  return m
+
+print(colum(n))
